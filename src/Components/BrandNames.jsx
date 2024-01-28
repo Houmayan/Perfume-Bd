8f191e1f93e8767f4d61f4736a17d35639544aea
houmayan <alichy101@gmail.com>
2024-01-29 01:32:56 +0600

    brand added and versace logo issue solved

--- src/Components/BrandNames.jsx
+++ src/Components/BrandNames.jsx
@@ -8,8 +8,8 @@ import mont from "../assets/brandImage/mont.svg";
 import dolce from "../assets/brandImage/dolce.svg";
 import bvlgari from "../assets/brandImage/bvlgari.svg";
 import gucci from "../assets/brandImage/gucci.svg";
-import versace1 from "../assets/brandImage/versace1.svg";
-import versace2 from "../assets/brandImage/versace2.svg";
+import versace1 from "../assets/brandImage/versace.svg";
+
 const BrandNames = () => {
   return (
     <div className="text-center mt-24">
@@ -37,35 +37,35 @@ const BrandNames = () => {
                     <ul className="inline-grid grid-cols-2 gap-x-10 gap-y-6 md:gap-x-16 md:grid-cols-3 lg:grid-cols-5 justify-center mx-auto items-center">
 
                         {/* LOGO 1 */}
-                      <li className="flex p-10 rounded-lg bg-[#efe2d2] h-28 w-56">
+                      <li className="flex px-10 rounded-lg bg-[#efe2d2] h-28 w-56">
                         <img className="mx-auto items-center"  src={ck} alt="" />
                       </li>
-                      <li className="flex p-10 rounded-lg bg-[#efe2d2] h-28 w-56">
+                      <li className="flex px-10  rounded-lg bg-[#efe2d2] h-28 w-56">
                         <img className="mx-auto items-center" src={armani} alt="" />
                       </li>
-                      <li className="flex p-10 rounded-lg bg-[#efe2d2] h-28 w-56">
+                      <li className="flex px-10 rounded-lg bg-[#efe2d2] h-28 w-56">
                         <img className="mx-auto items-center" src={dior} alt="" />
                       </li>
-                      <li className="flex p-10 rounded-lg bg-[#efe2d2] h-28 w-56">
+                      <li className="flex px-10 rounded-lg bg-[#efe2d2] h-28 w-56">
                         <img className="mx-auto items-center" src={chanel} alt="" />
                       </li>
-                      <li className="flex p-10 rounded-lg bg-[#efe2d2] h-28 w-56">
+                      <li className="flex px-10 rounded-lg bg-[#efe2d2] h-28 w-56">
                         <img className="mx-auto items-center" src={boss} alt="" />
                       </li>
-                      <li className="flex p-10 rounded-lg bg-[#efe2d2] h-28 w-56">
+                      <li className="flex px-10 rounded-lg bg-[#efe2d2] h-28 w-56">
                         <img className="mx-auto items-center" src={mont} alt="" />
                       </li>
-                      <li className="flex p-10 rounded-lg bg-[#efe2d2] h-28 w-56">
+                      <li className="flex px-10 rounded-lg bg-[#efe2d2] h-28 w-56">
                         <img className="mx-auto items-center" src={dolce} alt="" />
                       </li>
-                      <li className="flex p-10 rounded-lg flex-col items-center bg-[#efe2d2] h-28 w-56">
-                        <img className="w-12 mx-auto" src={versace1} alt="" />
-                        <img className="" src={versace2} alt="" />
+                      <li className="flex px-10 rounded-lg flex-col justify-center items-center bg-[#efe2d2] h-28 w-56">
+                        <img className=" mx-auto " src={versace1} alt="" />
+                       
                       </li>
-                      <li className="flex p-10 rounded-lg bg-[#efe2d2] h-28 w-56">
+                      <li className="flex px-10 rounded-lg bg-[#efe2d2] h-28 w-56">
                         <img className="mx-auto items-center" src={bvlgari} alt="" />
                       </li>
-                      <li className="flex p-10 rounded-lg bg-[#efe2d2] h-28 w-56">
+                      <li className="flex px-10 rounded-lg bg-[#efe2d2] h-28 w-56">
                         <img className="mx-auto items-center" src={gucci} alt="" />
                       </li>
 
